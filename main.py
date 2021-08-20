b12044cb6d4da00c70ce830b14d15b7d7caf124f
Jordan A. S. Kelly <jordandr3w@gmail.com>
2021-08-20 22:40:51 +0100

    Get Discovered connections Method Fix
    Small bug found when running the get Discovered connections method; It would occasionally fail to read a newly discovered value... This was mainly due the order/sequence of events. A watch() function for testing has been added. This simulates a full run through of a network flood. As expected - It grows exponentially and takes a while to clean the network.
    
    Way better than the previous attempt, which was using recursion, which quickly filled the machine stack.
    
    Next Tasks:
    - Research and begin implementing the API functionality

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ DATA_PACKET = {
     'HISTORY[x]': '',
     'MESSAGE': ''
 }
+ 
 
 def set_data() -> dict:
     for key in list(DATA_PACKET.keys()):
@@ -27,9 +28,7 @@ def set_data() -> dict:
             evaluate_str = eval(f"{needed_type}('{usr_input}')") 
             DATA_PACKET[key] = evaluate_str
 
-
     return DATA_PACKET
-
 
 if __name__ == '__main__':
 
@@ -61,20 +60,24 @@ if __name__ == '__main__':
                         # yield index, node_obj.node_id
                         yield index
 
+
         def find_depleted_packets(self):
             self.null_list = []
             for c in range(0, len(self.buffer)):
                 if self.buffer[c]['HOP_LIM'] == 0:
                     self.null_list.append(c)
             
+
         def remove_depleted_packets(self):
             for decrement, packet_index in enumerate(self.null_list):
                 self.buffer.pop(packet_index - decrement) 
+
 
         def next_hop(self):
             """ Send the packet to the next connections"""  
             print("I am <{}> and I'm connected to {}". format(self.node_id, self.connections))
 
+            self.get_new_connections()
             self.find_depleted_packets()
             self.remove_depleted_packets()
 
@@ -84,7 +87,7 @@ if __name__ == '__main__':
                 self.buffer[0]['HOP_LIM'] -= 1
 
                 for node_index in list(self.get_connected_node_obj()):
-
+                    print("I need to send to >>>>>", node_index)
                     if self.buffer[0]['HOP_LIM'] >= 0:
                         NODE_LIST[node_index].buffer.append(self.buffer[0].copy())         # Move value to the next object
 
@@ -98,21 +101,24 @@ if __name__ == '__main__':
         def get_new_connections(self):
 
             recent_node_list = []
-            new_connections = []
             history_list = list(str(self.buffer[i]['HISTORY[x]'])[:-1:] for i in range(self.get_congestion()))
-        
-            for i in range(self.get_congestion()):
-                index = str(str(self.buffer[i]['HISTORY[x]'])[::-1])[1::].index('>')                # Index of the last seperator
-                recent_node_list.append(str(history_list[i])[len(history_list[i]) - index::])       # Perform string slice and return latest Node ID
-
-            sorted_node_id_list = list(set(recent_node_list))   # Remove any duplicates
-
-            # Append any new connections to the discovery list
-            for node in sorted_node_id_list:        
-                if node not in self.connections:
-                    new_connections.append(node)
             
-            self.discovered_connections = new_connections
+            try:
+                for i in range(self.get_congestion()):
+                    index = str(str(self.buffer[i]['HISTORY[x]'])[::-1])[1::].index('>')                # Index of the last seperator
+                    recent_node_list.append(str(history_list[i])[len(history_list[i]) - index::])       # Perform string slice and return latest Node ID
+
+                sorted_node_id_list = list(set(recent_node_list))   # Remove any duplicates
+
+                # Append any new connections to the discovery list
+                for node in sorted_node_id_list:        
+                    if node not in self.connections and node not in self.discovered_connections:
+                        self.discovered_connections.append(node)
+
+            except ValueError:
+                """ Avoid a fail when the history is empty"""
+                pass
+
 
 
         def get_duplicates(self) -> int:
@@ -145,19 +151,30 @@ if __name__ == '__main__':
                 f'             /DUPLICATES:    {self.get_duplicates()}\n'
                 )
 
-    
-    packet_handler = []
 
+    def watch():
+        total = 0
+        while total != len(NODE_LIST):
+            total = 0
+            for x in range(len(NODE_LIST)):
+                NODE_LIST[x].next_hop()
+                print(NODE_LIST[x].get_node_info())
+                if NODE_LIST[x].get_congestion() == 0: 
+                    total += 1
+    
+    # Network Map
     NODE_LIST.append(Node('A', ['BBB', 'C']))
     NODE_LIST.append(Node('BBB', ['C', 'A']))
     NODE_LIST.append(Node('C', ['A', 'BBB', 'D']))
     NODE_LIST.append(Node('D', ['A', 'BBB']))
 
-    print(list(NODE_LIST[0].get_connected_node_obj()))
+    NODE_LIST[0].inject_packet(set_data())
+
+    watch()
 
 
-
-    debug = True
+    """Activate this to do a sequenced debug"""
+    debug = False
 
     if debug:
         print('########################################################### - NOTHING')
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,4 +1,1 @@
-            
-            print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>..', self.null_list)
-            # for j in self.null_list:
-            #     self.buffer.pop(j)
+connections
